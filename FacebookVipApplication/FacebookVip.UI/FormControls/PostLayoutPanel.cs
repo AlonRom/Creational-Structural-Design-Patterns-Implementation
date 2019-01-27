3845981c876d9db2bc2cfbbd810346b9dffe5fd0
igorg1990 <igorg@auroraview.com>
2019-01-27 22:27:28 +0200

    Revert "Strategy file writer"
    This reverts commit 9fe7b17981e15371f8b6e0589be9bbc4c1ea87dd.

--- FacebookVipApplication/FacebookVip.UI/FormControls/PostLayoutPanel.cs
+++ FacebookVipApplication/FacebookVip.UI/FormControls/PostLayoutPanel.cs
@@ -1,28 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using FacebookVip.Logic.FileWriter;
 using FacebookVip.Logic.Interfaces;
 using FacebookVip.Logic.Services;
-using FacebookVip.Model.FileWriter;
 using FacebookVip.Model.Models;
 using FacebookVip.UI.Properties;
 using FacebookWrapper.ObjectModel;
 
 namespace FacebookVip.UI.FormControls
 {
-
     public class PostLayoutPanel : ILayoutPanel
     {
         private ListBox UsersListBox { get; }
         private readonly TableLayoutPanel r_Panel;
         private readonly TableLayoutPanel r_PostsPanel;
-        private readonly Button r_HTML_buttom;
-        private readonly Button r_Dot_button;
 
         private int m_RowIndex;
 
@@ -35,8 +27,7 @@ namespace FacebookVip.UI.FormControls
                 Margin = new Padding(50, 20, 20, 20)
             };
 
-            r_PostsPanel = new TableLayoutPanel
-            {
+            r_PostsPanel = new TableLayoutPanel {
                 ColumnCount = 1,
                 AutoScroll = true,
                 Height = 300,
@@ -53,21 +44,6 @@ namespace FacebookVip.UI.FormControls
                 Dock = DockStyle.Fill
             };
 
-            r_HTML_buttom = new Button
-            {
-                Text = "Export HTML file",
-                Width = 120,
-                Margin = new Padding(50, 20, 20, 20)
-            };
-            r_HTML_buttom.Click += writePostsToHTMLFile;
-
-            r_Dot_button = new Button
-            {
-                Text = "Export Dot file",
-                Width = 120,
-                Margin = new Padding(50, 20, 20, 20)
-            };
-            r_Dot_button.Click += writePostsToDotFile;
         }
 
         public async Task<TableLayoutPanel> GetLayoutPanelAsync(User i_LoggedInUser)
@@ -75,7 +51,7 @@ namespace FacebookVip.UI.FormControls
             IFriendService friendsService = new FriendService();
             var friendsList = await friendsService.GetFriendsAsync(i_LoggedInUser);
             Label commentLabel = new Label { Text = Resources.SelectFriendWhosePOSTsYouLikeToSee, AutoSize = true };
-
+            
             UsersListBox.Items.Add(i_LoggedInUser);
             foreach (User user in friendsList)
             {
@@ -83,18 +59,16 @@ namespace FacebookVip.UI.FormControls
             }
             r_Panel.Controls.Add(commentLabel);
             r_Panel.Controls.Add(UsersListBox);
-            r_Panel.Controls.Add(r_HTML_buttom);
-            r_Panel.Controls.Add(r_Dot_button);
             UsersListBox.SelectedIndexChanged += personSelectedAsync;
 
-            UsersListBox.SelectedIndex = 0;
+            UsersListBox.SelectedIndex=0;
             personSelectedAsync(null, null);
             r_Panel.Controls.Add(r_PostsPanel);
 
-            return r_Panel;
+            return r_Panel; 
         }
 
-
+        
         private void addUsersPosts(List<PostModel> i_UserPosts)
         {
             foreach (PostModel post in i_UserPosts)
@@ -104,7 +78,6 @@ namespace FacebookVip.UI.FormControls
             }
         }
 
-        private List<PostModel> userPosts;
         private async void personSelectedAsync(object i_Sender, EventArgs i_EventArgs)
         {
             Cursor.Current = Cursors.WaitCursor;
@@ -114,9 +87,9 @@ namespace FacebookVip.UI.FormControls
             r_PostsPanel.Controls.Clear();
 
             foreach (User selectedUser in UsersListBox.SelectedItems)
-            {
+            { 
                 // add user data.
-                userPosts = await postService.GetUserPostsAsync(selectedUser);
+                List<PostModel> userPosts = await postService.GetUserPostsAsync(selectedUser);
                 if (userPosts.Count == 0)
                 {
                     userPosts = postService.GetUserPostsMockData(selectedUser);
@@ -126,22 +99,8 @@ namespace FacebookVip.UI.FormControls
             }
             UsersListBox.Enabled = true;
             Cursor.Current = Cursors.Arrow;
-
-           
-        }
-
-        void writePostsToDotFile(object i_Sender, EventArgs i_EventArgs)
-        {
-            FlieWriter dot_file_writer = new FlieWriter(new DotWriter());
-            dot_file_writer.WritePostsToFile(userPosts);
-        }
-
-        void writePostsToHTMLFile(object i_Sender, EventArgs i_EventArgs)
-        {
-            FlieWriter html_file_writer = new FlieWriter(new HTMLWriter());
-            html_file_writer.WritePostsToFile(userPosts);
         }
     }
 
-
+    
 }
