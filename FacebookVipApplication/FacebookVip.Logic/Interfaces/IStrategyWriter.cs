7e82a0e23e4189bd298866dd3602a0a80d6cef5d
igorg1990 <igorg@auroraview.com>
2019-01-27 22:45:14 +0200

    fix Strategy

---
+++ FacebookVipApplication/FacebookVip.Logic/Interfaces/IStrategyWriter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FacebookVip.Model.Models;
+using FacebookWrapper.ObjectModel;
+
+namespace FacebookVip.Logic.Interfaces
+{
+    public interface IStrategyWriter
+    {
+        string Extention { get; }
+        void Init();
+        void WriteLine(String line);
+        void Fini();
+        IEnumerable<string> GetResult();
+    }
+}
